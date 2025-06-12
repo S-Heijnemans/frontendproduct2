f63d4b8a5c35c32c6cbf2156507565298a1e6d2a
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-06-12 14:33:38 +0200

    Create-profile-eddit-profile

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import FetchData from "@/app/components/FetchData"
 import CreateData from "./components/CreateData";
 import LandingPage from "./page/LandingPage";
 import LoginPage from "./page/LoginPage";
+import ProfileShow from "./components/profile/ProfileShow";
 export default function Home() {
   return (
     <>
       <FetchData />
       <CreateData/>
       <LandingPage/>
+      <ProfileShow />
       <LoginPage/>
     </>
   );
